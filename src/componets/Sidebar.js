e0d52bdc1592ba61b617dec49236719e1dab6e19
Aditya8325-12 <adityadhutraj@gmail.com>
2024-10-22 16:17:48 +0530

    update

--- src/componets/Sidebar.js
+++ src/componets/Sidebar.js
@@ -1,24 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+
 const Sidebar = () => {
   const location = useLocation();
 
   const isActive = (path) => {
     return location.pathname === path
-      ? "text-blue-700  "
-      : "text-gray-900  md:hover:text-blue-700  ";
+      ? "text-blue-700"
+      : "text-gray-900 md:hover:text-blue-700";
   };
 
   return (
-    <form className="mt-4  border-gray-200">
-      <ul role="list" className="px-2 py-3 font-medium text-gray-900">
+    <form className="mt-4 border-gray-200">
+      <ul className="px-2 py-3 font-medium text-gray-900">
         <li>
           <Link
-            to={"/"}
-            className={`  block cursor-pointer hover:text-blue-400 px-2 py-3  ${isActive(
+            to="/"
+            className={`block cursor-pointer hover:text-blue-400 px-2 py-3 ${isActive(
               "/"
-            )} `}
+            )}`}
             aria-current={location.pathname === "/" ? "page" : undefined}
           >
             Home
@@ -26,40 +27,40 @@ const Sidebar = () => {
         </li>
         <li>
           <Link
-            to={"/user"}
-            className={`  block cursor-pointer hover:text-blue-400 px-2 py-3  ${isActive(
+            to="/user"
+            className={`block cursor-pointer hover:text-blue-400 px-2 py-3 ${isActive(
               "/user"
-            )} `}
+            )}`}
           >
             Users
           </Link>
         </li>
         <li>
           <Link
-            to={"/application"}
-            className={`  block cursor-pointer hover:text-blue-400 px-2 py-3  ${isActive(
+            to="/application"
+            className={`block cursor-pointer hover:text-blue-400 px-2 py-3 ${isActive(
               "/application"
-            )} `}
+            )}`}
           >
             Application
           </Link>
         </li>
         <li>
           <Link
-            to={"/notice"}
-            className={`  block cursor-pointer hover:text-blue-400 px-2 py-3  ${isActive(
+            to="/notice"
+            className={`block cursor-pointer hover:text-blue-400 px-2 py-3 ${isActive(
               "/notice"
-            )} `}
+            )}`}
           >
             Notice
           </Link>
         </li>
         <li>
           <Link
-            to={"/holiday"}
-            className={` block cursor-pointer hover:text-blue-400 px-2 py-3  ${isActive(
+            to="/holiday"
+            className={`block cursor-pointer hover:text-blue-400 px-2 py-3 ${isActive(
               "/holiday"
-            )} `}
+            )}`}
           >
             Holiday
           </Link>
